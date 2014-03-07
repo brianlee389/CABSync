ac18f63609298972180717f436b9ac2055783ab5
mirzael <michael.y.irizarry@gmail.com>
2014-03-07 16:50:58 -0500

    Mikes stuff for google calendar

--- gen/com/example/drexelcabapp/R.java
+++ gen/com/example/drexelcabapp/R.java
@@ -33,8 +33,7 @@ public final class R {
         public static final int title_refresh_progress=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int calendarlist=0x7f030001;
+        public static final int calendarlist=0x7f030000;
     }
     public static final class menu {
         public static final int main=0x7f070000;
